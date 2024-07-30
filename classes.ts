1fa1809e448ea35bfeceb6236946914a680a4a09
Maria Vittória Sousa Rodrigues Palma <138706978+m4vi@users.noreply.github.com>
2024-07-29 21:16:51 -0400

    Update classes.ts

--- classes.ts
+++ classes.ts
@@ -101,4 +101,69 @@ export class PJ
     }  
 }
 
+export class Gerente{
+    private _nome: string;
+    private _email: string;
+    private _senha: number;
+    private _numeroContato: number;
+    private _contasPF: number[];
 
+    constructor(nome: string, email: string, senha: number, numeroContato: number, contasPF: number[]){
+        this._nome = nome;
+        this._email = email;
+        this._senha = senha;
+        this._numeroContato = numeroContato;
+        this._contasPF = contasPF;
+    }
+
+    public getNome(): string{
+        return this._nome;
+    }
+
+    public setNome(nome: string): void {
+        this._nome = nome;
+    }
+
+    public getEmail(): string {
+        return this._email;
+    }
+
+    public setEmail(email: string): void {
+        this._email = email;
+    }
+
+    public getSenha(): number {
+        return this._senha;
+    }
+
+    public setSenha(senha: number): void {
+        this._senha = senha;
+    }
+
+    public getNumeroContato(): number {
+        return this._numeroContato;
+    }
+
+    public setNumeroContato(numeroContato: number): void {
+        this._numeroContato = numeroContato;
+    }
+
+    public getContaPF(numeroConta: number): number {
+        return this._contasPF[numeroConta];
+    }
+
+    public adicionarConta(numeroConta: number): void {
+        this._contasPF.push(numeroConta);
+    }
+
+    public removerConta(numeroConta: number): void {
+        const novoArray: number[] = [];
+        for (let i = 0; i < this._contasPF.length; i++) {
+            if (this._contasPF[i] !== numeroConta) {
+                novoArray.push(this._contasPF[i]);
+            }
+        }
+        this._contasPF = novoArray;
+    }
+
+}
