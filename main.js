461186b3f45edbdf7683a2d42ff5e9d3be088191
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-07-30 17:32:29 -0400

    gay3
    gay3

--- main.js
+++ main.js
@@ -17,13 +17,14 @@ function acessoMembros(arrayGerentes) {
         var procuraConta = function (email, arrayGerentes) {
             return arrayGerentes.findIndex(function (gerente) { return gerente.getEmail() === email; });
         };
-        var indiceUsuarioMembro = procuraConta(usuario, arrayGerentes);
+        indiceUsuarioMembro = procuraConta(usuario, arrayGerentes);
         console.log(arrayGerentes[indiceUsuarioMembro]);
         var inputSenha = parseInt(prompt("Senha da conta: "));
-        if (arrayGerentes[indiceUsuarioMembro].getSenha() !== inputSenha) {
-            console.log("\nSenha incorreta, operação reiniciada\n");
+        if (arrayGerentes[indiceUsuarioMembro].getSenha() !== inputSenha || indiceUsuarioMembro == -1) {
+            console.log("\nUsuário ou senha inválidos, operação reiniciada\n");
             continue;
         }
+        break;
     } while (true);
 }
 function login(arraycontasPF, arraycontasPJ) {
@@ -80,6 +81,7 @@ var arrayGerentes = [];
 //------------------------------------------------------------//
 var tipoConta = '\0';
 var indiceConta = 0;
+var indiceUsuarioMembro = 0;
 var valorTransferecia = 0;
 //------------------------------------------------------------//
 var user1 = new PJ_1.PJ("Mao Tse Tung", 12345678912345, 1234, 1, 850);
@@ -92,16 +94,17 @@ arraycontasPJ.push(user1, user2);
 arraycontasPF.push(user3, user4);
 arrayGerentes.push(user5);
 //------------------------------------------------------------//
+var escolha1;
+var escolha;
 do {
     console.log("-------------------------------------\n" +
         "|  Bem vindo ao Banco Aspili Getas  |\n" +
         "|  1 Login\n" +
         "|  2 Sou membro\n" +
-        "|  0 Sair");
-    var escolha1 = parseInt(prompt("Digite a opção desejada: "));
+        "|  0 Sair\n");
+    escolha1 = +prompt("Digite a opção desejada: ");
     if (escolha1 === 1) {
         login(arraycontasPF, arraycontasPJ);
-        break;
     }
     else if (escolha1 === 2) {
         acessoMembros(arrayGerentes);
@@ -112,127 +115,125 @@ do {
     else {
         console.log("\nFizeste cagada colega\n");
     }
-    break;
-} while (true);
-var escolha;
-do {
-    console.log("----------------------------\n" +
-        "| 1 Sacar dinheiro\n" +
-        "| 2 Depositar\n" +
-        "| 3 Extrato\n" +
-        "| 4 Transferencia\n" +
-        "| 5 Emprestimo\n" +
-        "| 6 Excluir conta\n" +
-        "| 0 Voltar\n" +
-        "----------------------------");
-    escolha = +prompt('Escolha: >> ');
-    switch (escolha) {
-        case 1:
-            //saque
-            if (tipoConta === "PF") {
-                var valorSaque = parseFloat(prompt("Digite o valor do saque: "));
-                arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorSaque);
-            }
-            else if (tipoConta === "PJ") {
-                var valorSaque = parseFloat(prompt("Digite o valor do saque: "));
-                arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorSaque);
-            }
-            break;
-        case 2:
-            //depositar
-            if (tipoConta === "PF") {
-                var valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
-                arraycontasPF[indiceConta].deposito(arraycontasPF, indiceConta, valorDeposito);
-            }
-            else if (tipoConta === "PJ") {
-                var valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
-                arraycontasPJ[indiceConta].deposito(arraycontasPJ, indiceConta, valorDeposito);
-            }
-            break;
-        case 3:
-            //extrato
-            if (tipoConta === "PF") {
-                arraycontasPF[indiceConta].infoPF(arraycontasPF, indiceConta);
-                //infoPF(contasPF:PF[],indice:number)
-            }
-            else if (tipoConta === "PJ") {
-                arraycontasPJ[indiceConta].infoPJ(arraycontasPJ, indiceConta);
-            }
-            break;
-        case 4:
-            //transferencia
-            var indiceContaRecebe = 0;
-            do {
-                indiceContaRecebe = 0;
-                var recebeTransferencia = +(prompt("Digite o pix da conta destinatária: "));
-                var tamanhoRecebeTransferencia = recebeTransferencia.toString().length;
-                if (tamanhoRecebeTransferencia === 11) {
-                    var procuraConta = function (cpf, arraycontas) {
-                        return arraycontas.findIndex(function (PF) { return PF.cpf === cpf; });
-                    };
-                    indiceContaRecebe = procuraConta(recebeTransferencia, arraycontasPF);
-                    if (indiceConta == -1) {
-                        console.log("\nConta não existente\n");
-                        continue;
-                    }
-                    valorTransferecia = parseInt(prompt("Digite o valor: "));
-                    if (valorTransferecia <= 0) {
-                        console.log("\nValor não correspondente\n");
-                    }
-                    if (tipoConta === "PF") {
-                        arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
-                    }
-                    else if (tipoConta === "PJ") {
-                        arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
-                    }
-                    arraycontasPF[indiceContaRecebe].deposito(arraycontasPF, indiceContaRecebe, valorTransferecia);
+    do {
+        console.log("----------------------------\n" +
+            "| 1 Sacar dinheiro\n" +
+            "| 2 Depositar\n" +
+            "| 3 Extrato\n" +
+            "| 4 Transferencia\n" +
+            "| 5 Emprestimo\n" +
+            "| 6 Excluir conta\n" +
+            "| 0 Voltar\n" +
+            "----------------------------");
+        escolha = +prompt('Escolha: >> ');
+        switch (escolha) {
+            case 1:
+                //saque
+                if (tipoConta === "PF") {
+                    var valorSaque = parseFloat(prompt("Digite o valor do saque: "));
+                    arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorSaque);
                 }
-                else if (tamanhoRecebeTransferencia === 14) {
-                    var procuraConta = function (cnpj, arraycontas) {
-                        return arraycontas.findIndex(function (PJ) { return PJ.cnpj === cnpj; });
-                    };
-                    indiceContaRecebe = procuraConta(recebeTransferencia, arraycontasPJ);
-                    if (indiceConta == -1) {
-                        console.log("\nConta não existente\n");
-                        continue;
-                    }
-                    valorTransferecia = parseInt(prompt("Digite o valor: "));
-                    if (valorTransferecia <= 0) {
-                        console.log("\nValor não correspondente\n");
-                    }
-                    if (tipoConta === "PF") {
-                        arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
-                    }
-                    else if (tipoConta === "PJ") {
-                        arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
-                    }
-                    arraycontasPJ[indiceContaRecebe].deposito(arraycontasPJ, indiceContaRecebe, valorTransferecia);
-                }
-                else {
-                    console.log("\nFizeste cagada\n");
-                    continue;
+                else if (tipoConta === "PJ") {
+                    var valorSaque = parseFloat(prompt("Digite o valor do saque: "));
+                    arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorSaque);
                 }
                 break;
-            } while (true);
-            break;
-        case 5:
-            //emprestimo
-            if (tipoConta === "PF") {
-            }
-            else if (tipoConta === "PJ") {
-            }
-            break;
-        case 6:
-            //excluir
-            if (tipoConta === "PF") {
-            }
-            else if (tipoConta === "PJ") {
-            }
-            break;
-        case 0:
-            break;
-        default:
-            console.log("Comando invalido, tente novamente");
-            break;
-    }
-} while (true);
+            case 2:
+                //depositar
+                if (tipoConta === "PF") {
+                    var valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
+                    arraycontasPF[indiceConta].deposito(arraycontasPF, indiceConta, valorDeposito);
+                }
+                else if (tipoConta === "PJ") {
+                    var valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
+                    arraycontasPJ[indiceConta].deposito(arraycontasPJ, indiceConta, valorDeposito);
+                }
+                break;
+            case 3:
+                //extrato
+                if (tipoConta === "PF") {
+                    arraycontasPF[indiceConta].infoPF(arraycontasPF, indiceConta);
+                    //infoPF(contasPF:PF[],indice:number)
+                }
+                else if (tipoConta === "PJ") {
+                    arraycontasPJ[indiceConta].infoPJ(arraycontasPJ, indiceConta);
+                }
+                break;
+            case 4:
+                //transferencia
+                var indiceContaRecebe = 0;
+                do {
+                    indiceContaRecebe = 0;
+                    var recebeTransferencia = +(prompt("Digite o pix da conta destinatária: "));
+                    var tamanhoRecebeTransferencia = recebeTransferencia.toString().length;
+                    if (tamanhoRecebeTransferencia === 11) {
+                        var procuraConta = function (cpf, arraycontas) {
+                            return arraycontas.findIndex(function (PF) { return PF.cpf === cpf; });
+                        };
+                        indiceContaRecebe = procuraConta(recebeTransferencia, arraycontasPF);
+                        if (indiceConta == -1) {
+                            console.log("\nConta não existente\n");
+                            continue;
+                        }
+                        valorTransferecia = parseInt(prompt("Digite o valor: "));
+                        if (valorTransferecia <= 0) {
+                            console.log("\nValor não correspondente\n");
+                        }
+                        if (tipoConta === "PF") {
+                            arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
+                        }
+                        else if (tipoConta === "PJ") {
+                            arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
+                        }
+                        arraycontasPF[indiceContaRecebe].deposito(arraycontasPF, indiceContaRecebe, valorTransferecia);
+                    }
+                    else if (tamanhoRecebeTransferencia === 14) {
+                        var procuraConta = function (cnpj, arraycontas) {
+                            return arraycontas.findIndex(function (PJ) { return PJ.cnpj === cnpj; });
+                        };
+                        indiceContaRecebe = procuraConta(recebeTransferencia, arraycontasPJ);
+                        if (indiceConta == -1) {
+                            console.log("\nConta não existente\n");
+                            continue;
+                        }
+                        valorTransferecia = parseInt(prompt("Digite o valor: "));
+                        if (valorTransferecia <= 0) {
+                            console.log("\nValor não correspondente\n");
+                        }
+                        if (tipoConta === "PF") {
+                            arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
+                        }
+                        else if (tipoConta === "PJ") {
+                            arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
+                        }
+                        arraycontasPJ[indiceContaRecebe].deposito(arraycontasPJ, indiceContaRecebe, valorTransferecia);
+                    }
+                    else {
+                        console.log("\nFizeste cagada\n");
+                        continue;
+                    }
+                    break;
+                } while (true);
+                break;
+            case 5:
+                //emprestimo
+                if (tipoConta === "PF") {
+                }
+                else if (tipoConta === "PJ") {
+                }
+                break;
+            case 6:
+                //excluir
+                if (tipoConta === "PF") {
+                }
+                else if (tipoConta === "PJ") {
+                }
+                break;
+            case 0:
+                break;
+            default:
+                console.log("Comando invalido, tente novamente");
+                break;
+        }
+    } while (escolha != 0);
+} while (escolha1 != 0);
--- main.ts
+++ main.ts
@@ -116,24 +116,23 @@ arrayGerentes.push(user5);
 
 //------------------------------------------------------------//
 
-
-
+let escolha1:number
+let escolha: number;
 
 do{
 
     console.log("-------------------------------------\n" +
         "|  Bem vindo ao Banco Aspili Getas  |\n" +
         "|  1 Login\n"+
-        "|  2 Sou membro\n"+
-        "|  0 Sair");
+        "|  2 Sou membro\n" +
+        "|  0 Sair\n");
 
-
-    let escolha1:number=parseInt(prompt("Digite a opção desejada: "));
+     escolha1 = +prompt("Digite a opção desejada: ");
 
     if(escolha1===1){
         login(arraycontasPF, arraycontasPJ);
 
-        break;
+        
     }
     else if(escolha1===2){
         acessoMembros(arrayGerentes);
@@ -142,172 +141,171 @@ do{
     }else{
         console.log("\nFizeste cagada colega\n");
     }
-    break;
-}while(true);
-
-let escolha: number;
+    
 
 
-
-
-
-do {
-    console.log("----------------------------\n" +
-        "| 1 Sacar dinheiro\n" +
-        "| 2 Depositar\n" +
-        "| 3 Extrato\n" +
-        "| 4 Transferencia\n" +
-        "| 5 Emprestimo\n" +
-        "| 6 Excluir conta\n" +
-        "| 0 Voltar\n" +
-        "----------------------------");
-
-    escolha = +prompt('Escolha: >> ');
-
-    switch (escolha) {
-        case 1:
-            //saque
-            if(tipoConta==="PF"){
-                const valorSaque = parseFloat(prompt("Digite o valor do saque: "));
-                arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorSaque);
-            }else if(tipoConta==="PJ"){
-                const valorSaque = parseFloat(prompt("Digite o valor do saque: "));
-                arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorSaque);
-            }
-
-            break;
-
-        case 2:
-            //depositar
-            if(tipoConta==="PF"){
-                const valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
-                arraycontasPF[indiceConta].deposito(arraycontasPF, indiceConta, valorDeposito);
-            }else if(tipoConta==="PJ"){
-                const valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
-                arraycontasPJ[indiceConta].deposito(arraycontasPJ, indiceConta, valorDeposito);
-            }
-
-
-            break;
-
-        case 3:
-            //extrato
-            if(tipoConta==="PF"){
-                arraycontasPF[indiceConta].infoPF(arraycontasPF, indiceConta);
-                //infoPF(contasPF:PF[],indice:number)
-            }else if(tipoConta==="PJ"){
-                arraycontasPJ[indiceConta].infoPJ(arraycontasPJ, indiceConta);
-            }
-
-
-            break;
-
-        case 4:
-            //transferencia
-            var indiceContaRecebe:number = 0;
-
-            do {
-                indiceContaRecebe= 0;
-
-                let recebeTransferencia:number=+(prompt("Digite o pix da conta destinatária: "));
-                let tamanhoRecebeTransferencia:number=recebeTransferencia.toString().length;
-
-                if(tamanhoRecebeTransferencia===11){
-                    const procuraConta = (cpf: number, arraycontas: PF[]): number => {
-                        return arraycontas.findIndex((PF) => PF.cpf === cpf);
-                    }
-                        indiceContaRecebe=procuraConta(recebeTransferencia,arraycontasPF);
-
-                    if (indiceConta == -1) {
-                        console.log("\nConta não existente\n");
-                        continue;
-                    }
-
-                    valorTransferecia=parseInt(prompt("Digite o valor: "));
-
-                    if(valorTransferecia<=0){
-                        console.log("\nValor não correspondente\n");
-                    }
-
-
-                    if(tipoConta==="PF"){
-                        arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
-                    }else if(tipoConta==="PJ"){
-                        arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
-                    }
-
-
-                    arraycontasPF[indiceContaRecebe].deposito(arraycontasPF, indiceContaRecebe, valorTransferecia);
-
-                }else if(tamanhoRecebeTransferencia===14){
-                    const procuraConta = (cnpj: number, arraycontas: PJ[]): number => {
-                        return arraycontas.findIndex((PJ) => PJ.cnpj === cnpj);
-                    }
-                    indiceContaRecebe=procuraConta(recebeTransferencia,arraycontasPJ);
-
-                    if (indiceConta == -1) {
-                        console.log("\nConta não existente\n");
-                        continue;
-                    }
-
-                    valorTransferecia=parseInt(prompt("Digite o valor: "));
-
-                    if(valorTransferecia<=0){
-                        console.log("\nValor não correspondente\n");
-                    }
-
-
-                    if(tipoConta==="PF"){
-                        arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
-                    }else if(tipoConta==="PJ"){
-                        arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
-                    }
-
-
-                    arraycontasPJ[indiceContaRecebe].deposito(arraycontasPJ, indiceContaRecebe, valorTransferecia);
-                }else{
-                    console.log("\nFizeste cagada\n");
-                    continue;
-                }
-                break;
-            }while (true);
-
-
-            break;
-
-        case 5:
-            //emprestimo
-            if(tipoConta==="PF"){
-
-
-            }else if(tipoConta==="PJ"){
-
-        }
-
-
-            break;
-
-        case 6:
-            //excluir
-            if(tipoConta==="PF"){
-
-            }else if(tipoConta==="PJ"){
-
-        }
-
-
-            break;
-
-        case 0:
-
-            break;
-
-        default:
+    do {
+        console.log("----------------------------\n" +
+            "| 1 Sacar dinheiro\n" +
+            "| 2 Depositar\n" +
+            "| 3 Extrato\n" +
+            "| 4 Transferencia\n" +
+            "| 5 Emprestimo\n" +
+            "| 6 Excluir conta\n" +
+            "| 0 Voltar\n" +
+            "----------------------------");
             
-            console.log("Comando invalido, tente novamente");
-            break;
-    }
+            escolha = +prompt('Escolha: >> ');
 
-} while (true);
+        switch (escolha) {
+            case 1:
+                //saque
+                if(tipoConta==="PF"){
+                    const valorSaque = parseFloat(prompt("Digite o valor do saque: "));
+                    arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorSaque);
+                }else if(tipoConta==="PJ"){
+                    const valorSaque = parseFloat(prompt("Digite o valor do saque: "));
+                    arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorSaque);
+                }
+    
+                break;
+    
+            case 2:
+                //depositar
+                if(tipoConta==="PF"){
+                    const valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
+                    arraycontasPF[indiceConta].deposito(arraycontasPF, indiceConta, valorDeposito);
+                }else if(tipoConta==="PJ"){
+                    const valorDeposito = parseFloat(prompt("Digite o valor do deposito: "));
+                    arraycontasPJ[indiceConta].deposito(arraycontasPJ, indiceConta, valorDeposito);
+                }
+    
+    
+                break;
+    
+            case 3:
+                //extrato
+                if(tipoConta==="PF"){
+                    arraycontasPF[indiceConta].infoPF(arraycontasPF, indiceConta);
+                    //infoPF(contasPF:PF[],indice:number)
+                }else if(tipoConta==="PJ"){
+                    arraycontasPJ[indiceConta].infoPJ(arraycontasPJ, indiceConta);
+                }
+    
+    
+                break;
+    
+            case 4:
+                //transferencia
+                var indiceContaRecebe:number = 0;
+    
+                do {
+                    indiceContaRecebe= 0;
+    
+                    let recebeTransferencia:number=+(prompt("Digite o pix da conta destinatária: "));
+                    let tamanhoRecebeTransferencia:number=recebeTransferencia.toString().length;
+    
+                    if(tamanhoRecebeTransferencia===11){
+                        const procuraConta = (cpf: number, arraycontas: PF[]): number => {
+                            return arraycontas.findIndex((PF) => PF.cpf === cpf);
+                        }
+                            indiceContaRecebe=procuraConta(recebeTransferencia,arraycontasPF);
+    
+                        if (indiceConta == -1) {
+                            console.log("\nConta não existente\n");
+                            continue;
+                        }
+    
+                        valorTransferecia=parseInt(prompt("Digite o valor: "));
+    
+                        if(valorTransferecia<=0){
+                            console.log("\nValor não correspondente\n");
+                        }
+    
+    
+                        if(tipoConta==="PF"){
+                            arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
+                        }else if(tipoConta==="PJ"){
+                            arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
+                        }
+    
+    
+                        arraycontasPF[indiceContaRecebe].deposito(arraycontasPF, indiceContaRecebe, valorTransferecia);
+    
+                    }else if(tamanhoRecebeTransferencia===14){
+                        const procuraConta = (cnpj: number, arraycontas: PJ[]): number => {
+                            return arraycontas.findIndex((PJ) => PJ.cnpj === cnpj);
+                        }
+                        indiceContaRecebe=procuraConta(recebeTransferencia,arraycontasPJ);
+    
+                        if (indiceConta == -1) {
+                            console.log("\nConta não existente\n");
+                            continue;
+                        }
+    
+                        valorTransferecia=parseInt(prompt("Digite o valor: "));
+    
+                        if(valorTransferecia<=0){
+                            console.log("\nValor não correspondente\n");
+                        }
+    
+    
+                        if(tipoConta==="PF"){
+                            arraycontasPF[indiceConta].saque(arraycontasPF, indiceConta, valorTransferecia);
+                        }else if(tipoConta==="PJ"){
+                            arraycontasPJ[indiceConta].saque(arraycontasPJ, indiceConta, valorTransferecia);
+                        }
+    
+    
+                        arraycontasPJ[indiceContaRecebe].deposito(arraycontasPJ, indiceContaRecebe, valorTransferecia);
+                    }else{
+                        console.log("\nFizeste cagada\n");
+                        continue;
+                    }
+                    break;
+                }while (true);
+    
+    
+                break;
+    
+            case 5:
+                //emprestimo
+                if(tipoConta==="PF"){
+    
+    
+                }else if(tipoConta==="PJ"){
+    
+            }
+    
+    
+                break;
+    
+            case 6:
+                //excluir
+                if(tipoConta==="PF"){
+    
+                }else if(tipoConta==="PJ"){
+    
+            }
+    
+    
+                break;
+    
+            case 0:
+                
+                break;
+    
+            default:
+                
+                console.log("Comando invalido, tente novamente");
+                break;
+        }
+    
+    } while (escolha != 0);
+
+
+}while(escolha1 != 0);
+
+
 
 export {}
